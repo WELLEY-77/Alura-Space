2e4a2ebaca8f032b5a6ff0ac2ff76f32db392029
Welley <monteirowelley7@gmail.com>
2023-02-10 14:48:49 -0300

    criandno rota

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from galeria.views import galeria
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('galeria', include('galeria.urls')), outra forma de fazer.
-    path('', galeria), 
+    path('galeria/', include('galeria.urls'))
 ]
