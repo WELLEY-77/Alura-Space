91239eaa68895ef4534e82fc5fe085e922145b03
Welley <monteirowelley7@gmail.com>
2023-02-10 15:03:47 -0300

    refatorando a funcao

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def galeria(request):
-    return HttpResponse('<h1>Alura Space</h1>')
+    return render(request, 'galeria.html')
