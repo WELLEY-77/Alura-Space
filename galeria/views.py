586f277ed091ee6e197726b50d81d5afa5a4240d
Welley <monteirowelley7@gmail.com>
2023-02-10 15:25:31 -0300

    alterando url

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def galeria(request):
-    return render(request, 'galeria.html')
+    return render(request, 'galeria/galeria.html')
