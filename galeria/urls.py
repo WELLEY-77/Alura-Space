6d2520669c810dee09fc00d482f8521b48c59d8f
Welley <monteirowelley7@gmail.com>
2023-03-06 16:11:51 -0300

    add urls e images

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.galeria, name='home'),
-    path('image/', views.image, name='image'),
+    path('image/<int:foto_id>', views.image, name='image'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Fotografia
 
@@ -12,5 +12,7 @@ def galeria(request):
 
     return render(request, 'galeria/galeria.html', dados)
 
-def image(request):
-    return render(request, 'galeria/image.html')
+def image(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+
+    return render(request, 'galeria/image.html', {'fotografia':fotografia})
