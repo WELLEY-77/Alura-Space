cc3a9cc5bb7f265c6a6870140d7712ad159203a3
Welley <monteirowelley7@gmail.com>
2023-03-06 16:43:00 -0300

    add nova foto, and conectar o admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Fotografia
 
-# Register your models here.
+class ListaFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+admin.site.register(Fotografia, ListaFotografias)
