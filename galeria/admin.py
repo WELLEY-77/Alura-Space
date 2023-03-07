088daba890be3558f45c3c690b948cc25aafec39
Welley <monteirowelley7@gmail.com>
2023-03-07 11:05:13 -0300

    ajeitando admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,5 +5,7 @@ class ListaFotografias(admin.ModelAdmin):
     list_display = ('id', 'nome', 'legenda')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListaFotografias)
