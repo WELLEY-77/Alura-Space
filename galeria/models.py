00ae1d2a86666845fe334884c1c891ece9695da9
Welley <monteirowelley7@gmail.com>
2023-03-07 10:55:31 -0300

    criando novo campo no models

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Fotografia(models.Model):
+
+    OPCOES_CATEGORIAS = [
+        ('ESTRELA', 'Estrela'),
+        ('PLANETA', 'Planeta'),
+        ('GALÁXIA', 'Galáxia'),
+        ('NEBULOSA', 'Nebulosa'),
+    ]
+
     nome = models.CharField(max_length=50, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, default='',choices=OPCOES_CATEGORIAS )
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
