8a1335c58202cf1839218e3145446e5781ce20b4
Welley <monteirowelley7@gmail.com>
2023-03-09 14:33:24 -0300

    criando os templates do novo app

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
-    path('usuarios/', include('usuarios.urls')),
+    path('', include('usuarios.urls')),
 
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
