e0378e27c9a8295f7689ac8e6ce33ec4d7351076
Welley <monteirowelley7@gmail.com>
2023-03-09 15:00:47 -0300

    criando os templates do novo app, e lincando com o menu

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login),
-    path('cadastro/', views.cadastro),
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 ]
