a781a000c19cbd2c84237720e6cde161ec4feea9
Welley <monteirowelley7@gmail.com>
2023-03-09 14:18:15 -0300

    apresentando app, e a url do app ao core

--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galeria.urls'))
+    path('', include('galeria.urls')),
+    path('usuarios/', include('usuarios.urls')),
+
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
