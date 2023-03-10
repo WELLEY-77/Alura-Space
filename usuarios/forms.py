4d1254284b0915577940248a45f7b8b9c355d796
Welley <monteirowelley7@gmail.com>
2023-03-10 15:39:36 -0300

    criando forms para o projeto

---
+++ usuarios/forms.py
@@ -0,0 +1,76 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        max_length=100,
+        label='Nome do login',
+        required=True,
+        widget= forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Ex.: João Silva'
+            }
+        ),
+    )
+
+    senha = forms.CharField(
+        max_length=70,
+        label='Senha',
+        required=True,
+        widget= forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Digite sua senha',
+            }
+        ),
+    )
+
+class CadastoForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        max_length=100,
+        required=True,
+        label='Nome de Cadastro',
+        widget= forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Ex.: João Silva'
+            }
+        )  
+    )
+
+    email = forms.EmailField(
+        max_length=100,
+        label='E-mail',
+        required=True,
+        widget= forms.EmailInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Ex.: joaosilva@xpto.com',
+            }
+        )
+    )
+
+    senha_1 = forms.CharField(
+        max_length=70,
+        label='Senha',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Digite sua senha',
+            }
+        )
+    )
+
+    senha_2 = forms.CharField(
+        max_length=70,
+        required=True,
+        label='Confirme sua senha',
+        widget= forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Digite sua senha novamente',
+            }
+        )
+    )
+
