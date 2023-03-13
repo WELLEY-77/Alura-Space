709bd14a9ded6aeedb79de3a63be5a3a7df15fca
Welley <monteirowelley7@gmail.com>
2023-03-13 15:00:29 -0300

    fazendo as validaçoes dos formularios, e passando os dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import LoginForms, CadastoForms
+from django.contrib.auth.models import User
 
 def login(request):
 
@@ -10,5 +11,27 @@ def login(request):
 def cadastro(request):
 
     form = CadastoForms()
+
+    if request.method == 'POST':
+        form = CadastoForms(request.POST)
+
+    if form.is_valid():
+        if form['senha_1'].value() != form['senha_2'].value():
+            return redirect('cadastro')
+        
+        nome = form['nome_cadastro'].value()
+        email = form['email'].value()
+        senha= form['senha_1'].value()
+        
+        if User.objects.filter(username=nome).exists():
+            return redirect('cadastro')
+        
+        usuario = User.objects.create_user(
+            username=nome,
+            email=email,
+            password=senha,
+        )
+        usuario.save()
+        return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {'form':form})
