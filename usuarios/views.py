a45f3d15a942734af92f7a9a648ba193c3481aa0
Welley <monteirowelley7@gmail.com>
2023-03-14 14:25:50 -0300

    fazendo validaçoes do login de usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render,redirect
 from .forms import LoginForms, CadastoForms
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 def login(request):
 
     form = LoginForms()
+
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+
+            usuario = auth.authenticate(
+                request,
+                username=nome,
+                password = senha,
+            )
+
+            if usuario is not None:
+                auth.login(request, usuario)
+                return redirect('home')
+            else:
+                return redirect('login')
+
 
     return render(request, 'usuarios/login.html', {'form':form})
 
