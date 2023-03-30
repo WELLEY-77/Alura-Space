57c4e0a68c8ae6b025ccf8a62a3a1c04928cf677
Welley <monteirowelley7@gmail.com>
2023-03-30 14:46:35 -0300

    pegando parametro de id para mostarr na url

--- galeria/urls.py
+++ galeria/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('image/<int:foto_id>', views.image, name='image'),
     path('buscar/', views.buscar, name='buscar'),
     path('nova-imagem', views.nova_imagem, name='nova_imagem'),
-    path('editar-imagem', views.editar_imagem, name='editar_imagem'),
+    path('editar-imagem/<int:foto_id>', views.editar_imagem, name='editar_imagem'),
     path('deletar-imagem', views.deletar_imagem, name='deletar_imagem'),
 ]
