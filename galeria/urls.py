896fbd58c4bc41742919571c7948b8a241e89f85
Welley <monteirowelley7@gmail.com>
2023-03-30 15:03:10 -0300

    deleção de uma fotografia

--- galeria/urls.py
+++ galeria/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('buscar/', views.buscar, name='buscar'),
     path('nova-imagem', views.nova_imagem, name='nova_imagem'),
     path('editar-imagem/<int:foto_id>', views.editar_imagem, name='editar_imagem'),
-    path('deletar-imagem', views.deletar_imagem, name='deletar_imagem'),
+    path('deletar-imagem/<int:foto_id>', views.deletar_imagem, name='deletar_imagem'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -76,8 +76,12 @@ def editar_imagem(request, foto_id):
     return render(request, 'galeria/editar_imagem.html', dados)
 
 
-def deletar_imagem(request):
-    pass
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Fotografia deletada com suceso!')
+
+    return redirect('home')
 
 
 
